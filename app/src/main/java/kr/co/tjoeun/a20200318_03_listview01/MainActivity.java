5ae9510b5845e8d00582e732bfc2b4d506497c6a
potential20 <passion212@nate.com>
2020-03-18 12:40:46 +0900

    목록 / 어댑터 변수 객체화  - 리스트뷰와 연결  - 목록에 가게 추가 => 그만큼 리스트 갯수도 추가 확인

--- app/src/main/java/kr/co/tjoeun/a20200318_03_listview01/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200318_03_listview01/MainActivity.java
@@ -5,9 +5,19 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.co.tjoeun.a20200318_03_listview01.adapters.StoreAdapter;
 import kr.co.tjoeun.a20200318_03_listview01.databinding.ActivityMainBinding;
+import kr.co.tjoeun.a20200318_03_listview01.datas.Store;
 
 public class MainActivity extends BaseActivity {
+
+//    가게목록이 담길 리스트
+    List<Store> storeDatas = new ArrayList<>();
+//    목록을 뿌려줄 StoreAdapter 변수
+    StoreAdapter storeAdapter = null;
 
     ActivityMainBinding binding = null;
 
@@ -26,6 +36,19 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+
+        addStores();
+
+//        어댑터를 객체화 (우리가 작성한 생성자 활용)
+        storeAdapter = new StoreAdapter(mContext, R.layout.store_list_item,storeDatas);
+//        리스트뷰와 어댑터를 연결
+        binding.storeListView.setAdapter(storeAdapter);
+
+    }
+
+    void addStores() {
+        storeDatas.add(new Store("삼겹더하기 본점",4));
+        storeDatas.add(new Store("밥해주는 남자",3));
 
     }
 }
